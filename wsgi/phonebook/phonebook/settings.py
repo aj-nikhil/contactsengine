9df4274260d0ba435b4a2849f35600bc231cd4ba
Nikhil <nikhilmanahs@gmail,com>
2013-10-12 14:45:33 +0530

    mmedia and static fix

--- wsgi/phonebook/phonebook/settings.py
+++ wsgi/phonebook/phonebook/settings.py
@@ -50,7 +50,7 @@ MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = 'C:/Users/Nikhil/phonebook/media/'
+MEDIA_ROOT = SITE_ROOT + 'phonebook/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -61,7 +61,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = 'C:/Users/Nikhil/phonebook/static/'
+STATIC_ROOT = SITE_ROOT + 'fonebuk/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -121,7 +121,7 @@ ROOT_URLCONF = 'phonebook.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'phonebook.wsgi.application'
 
-TEMPLATE_DIRS = ("C:/Users/Nikhil/phonebook/fonebuk/templates",
+TEMPLATE_DIRS = ( SITE_ROOT  + "fonebuk/templates",
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
