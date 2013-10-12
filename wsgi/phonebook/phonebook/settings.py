d21fd7a6d9a47636e943b6a9ad2b3739795d91dd
Nikhil <nikhilmanahs@gmail,com>
2013-10-12 14:39:08 +0530

    db filepath change

--- wsgi/phonebook/phonebook/settings.py
+++ wsgi/phonebook/phonebook/settings.py
@@ -13,7 +13,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': "new.db",  # Or path to database file if using sqlite3.
+        'NAME': "/var/lib/openshift/52590bd44382ec937f00003b/app-root/repo/wsgi/phonebook/new.db",  # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
