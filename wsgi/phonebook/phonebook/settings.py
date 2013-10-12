a9ac7210f41c81390fc7b99e052901543ce69505
Nikhil <nikhilmanahs@gmail,com>
2013-10-12 14:47:28 +0530

    mmedia and static fix again

--- wsgi/phonebook/phonebook/settings.py
+++ wsgi/phonebook/phonebook/settings.py
@@ -61,7 +61,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = SITE_ROOT + 'fonebuk/static/'
+STATIC_ROOT = SITE_ROOT + 'phonebook/fonebuk/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -121,7 +121,7 @@ ROOT_URLCONF = 'phonebook.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'phonebook.wsgi.application'
 
-TEMPLATE_DIRS = ( SITE_ROOT  + "fonebuk/templates",
+TEMPLATE_DIRS = ( SITE_ROOT  + "phonebook/fonebuk/templates",
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
